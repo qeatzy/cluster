ed374ba0dc59f5eecf68f9ca8ac639b34f886770
U-QEATZY\Dell <qeatzy@users.noreply.github.com>
2017-03-30 09:58:17 +0800

    add file kmeans.cpp

---
+++ kmeans.cpp
@@ -0,0 +1,11 @@
+#include "utility.h"
+// kmeans, kmeans++, etc.
+// NP-hard kmeans problem, standard kmeans problem or floyd algorithm.
+// seeding or set initial state, partition re-partition.
+// https://en.wikipedia.org/wiki/K-means++
+// https://en.wikipedia.org/wiki/Lloyd's_algorithm
+
+
+int main() {
+    test::init();
+}
